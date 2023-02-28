11a015d54852cf7b75b2e0995b3a85a91615c232
saktbv <112687531+saktbv@users.noreply.github.com>
2023-02-28 12:44:50 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -6,5 +6,6 @@ class Main{
     System.out.println("Hello World pt.3");    
     System.out.println("Hello World pt.4");    
     System.out.println("Hello World pt.5");    
+    System.out.println("Hello World pt.6");    
 }
 }
